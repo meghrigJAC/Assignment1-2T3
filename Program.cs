c92afd88f459203549756516f8509e7a52fddea7
Meghrig <76222303+meghrigJAC@users.noreply.github.com>
2023-01-27 13:05:00 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment1_2T3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
